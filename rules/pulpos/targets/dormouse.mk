9cacb23447b069723c440335b409870118a3ea03
Luca Rufer <luca@mosaic-soc.com>
2024-12-27 14:03:15 +0100

    Add Dormouse dual core linker script

--- rules/pulpos/targets/dormouse.mk
+++ rules/pulpos/targets/dormouse.mk
@@ -4,13 +4,17 @@ PULP_CFLAGS += -D__cv32e40p__ -U__riscv__ -UARCHI_CORE_HAS_PULPV2
 
 PULP_ARCH_CFLAGS    ?= -march=rv32imac_zfinx_xcorev -mabi=ilp32 -mno-pulp-hwloop
 PULP_ARCH_LDFLAGS   ?= -march=rv32imac_zfinx_xcorev -mabi=ilp32 -mno-pulp-hwloop
-PULP_ARCH_OBJDFLAGS ?= 
+PULP_ARCH_OBJDFLAGS ?=
 
 PULP_CFLAGS    += -fdata-sections -ffunction-sections -include chips/dormouse/config.h -I$(PULPRT_HOME)/include/chips/dormouse
 PULP_OMP_CFLAGS    += -fopenmp -mnativeomp
 PULP_LDFLAGS += -nostartfiles -nostdlib -Wl,--gc-sections -L$(PULPRT_HOME)/kernel -lgcc
 
+ifeq ($(DORMOUSE_DUALCORE), 1)
+PULP_LDFLAGS += -Tchips/dormouse/link_dual_core.ld
+else
 PULP_LDFLAGS += -Tchips/dormouse/link.ld
+endif
 
 PULP_CC = riscv32-unknown-elf-gcc
 PULP_AR ?= riscv32-unknown-elf-ar
